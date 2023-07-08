0a4efa2743bb83669ada6ec502aeff2a00863b2f
sidee <said.issack@student.moringaschool.com>
2023-07-08 15:18:19 +0300

    added the let function on js

--- app.js
+++ app.js
@@ -19,3 +19,90 @@ fetch('db.json')
   .catch(error => {
     console.log('Error:', error);
   });
+
+
+  let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', () => {
+  body.classList.add('active');
+});
+
+closeShopping.addEventListener('click', () => {
+  body.classList.remove('active');
+});
+
+let products = [
+  {
+    "id": 1,
+    "name": "Apples",
+    "category": "Fruits",
+    "price": 1.99,
+    "quantity": 10,
+    "image": "images/apple.png"
+  },
+  {
+    "id": 2,
+    "name": "Bananas",
+    "category": "Fruits",
+    "price": 0.99,
+    "quantity": 15,
+    "image": "images/banana.png"
+  },
+  {
+    "id": 3,
+    "name": "Milk",
+    "category": "Dairy",
+    "price": 2.49,
+    "quantity": 5,
+    "image": "images/milk.png"
+  },
+  {
+    "id": 4,
+    "name": "Eggs",
+    "category": "Dairy",
+    "price": 1.79,
+    "quantity": 12,
+    "image": "images/eggs.png"
+  },
+  {
+    "id": 5,
+    "name": "Bread",
+    "category": "Bakery",
+    "price": 2.99,
+    "quantity": 8,
+    "image": "images/bread.png"
+  },
+  {
+    "id": 6,
+    "name": "Cheese",
+    "category": "Meat",
+    "price": 4.99,
+    "quantity": 3,
+    "image": "images/cheese.png"
+  }
+];
+let listCards = [];
+
+function addToCard(id) {
+  let selectedProduct = products.find(product => product.id === id);
+
+  if (selectedProduct) {
+    let existingItem = listCards.find(item => item.id === id);
+    if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        listCards.push({
+          ...selectedProduct,
+          quantity: 1
+        });
+      }
+  
+      reloadCard();
+    }
+  }
